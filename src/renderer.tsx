27ad6aa1be2455a186c10bcef606ef4e855a4c97
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-02-24 18:25:29 +0900

    feat: react-works example

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,22 @@
-import { jsxRenderer } from 'hono/jsx-renderer'
+import { jsxRenderer } from "hono/jsx-renderer";
 
+// これでいけるから、うまいこと毎回rendererにオプションでscriptのパスを与えたら動作しそう。
+// でも、その仕組み作るのもめんどくさいし、トランスパイル後のjsファイルの配置先も覚えないといけないので辛い。
 export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
         <link href="/static/style.css" rel="stylesheet" />
+        {import.meta.env.PROD ? (
+          <script type="module" src="/static/client.js" />
+        ) : (
+          <script type="module" src="/examples/spa/client.tsx" />
+        )}
       </head>
-      <body>{children}</body>
+      <body>
+        <div id="root" />
+        {children}
+      </body>
     </html>
-  )
-})
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,35 @@ import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: "src/index.tsx",
-    }),
-  ],
-  optimizeDeps: {
-    entries: [],
-  },
-  test: {
-    globals: true,
-  },
+export default defineConfig(({ mode }) => {
+  if (mode === "client")
+    return {
+      esbuild: {
+        jsxImportSource: "hono/jsx/dom", // Optimized for hono/jsx/dom
+      },
+      build: {
+        rollupOptions: {
+          input: "./src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    };
+
+  return {
+    plugins: [
+      build(),
+      devServer({
+        adapter,
+        entry: "src/index.tsx",
+      }),
+    ],
+    optimizeDeps: {
+      entries: [],
+    },
+    test: {
+      globals: true,
+    },
+  };
 });
