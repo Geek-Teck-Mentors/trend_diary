e6ca67605c9f228861e9b40fe9f576182ec83b7e
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-03-18 00:30:17 +0900

    参考リンク追加

--- src/infrastructure/rdb.ts
+++ src/infrastructure/rdb.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 // 開発環境では標準クライアント、本番環境ではエッジクライアントを使用
+// 参考：https://hono.dev/examples/prisma
 export default function getRdbClient(databaseUrl: string) {
   if (process.env.NODE_ENV === 'development') {
     const devPrisma = new PrismaClientLocal({
