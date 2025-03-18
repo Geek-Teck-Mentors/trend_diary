4da7ff4243010e8bc852a35357fdd69a1a97bf4d
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-03-18 23:55:21 +0900

    fix: 不要なマイグレーションの削除

--- prisma/migrations/20250318145435_add_user/migration.sql
+++ prisma/migrations/20250318145435_add_user/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "user_id" SERIAL NOT NULL,
-    "account_id" INTEGER NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "account_id" TEXT NOT NULL,
     "display_name" TEXT NOT NULL,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP NOT NULL,
