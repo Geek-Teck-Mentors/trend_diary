b40fb000777a1bc8b502b9e4360ad3ab323851fd
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-03-20 19:19:06 +0900

    fix: backend coverage thresholds

--- vitest.config.backend.ts
+++ vitest.config.backend.ts
@@ -11,6 +11,7 @@ export default defineConfig({
         statements: 60, // 命令網羅, ソースコードの全ての命令が実行されるかどうか
         branches: 60, // 分岐網羅, 処理のパスの通過率とほぼ同義
         functions: 60, // 関数網羅, 関数の実行パスの通過率
+        lines: 60, // 行網羅, ソースコードの全ての行が実行されるかどうか
       },
       include: ['src/**/*'],
       exclude: ['src/app/**/*', 'src/middleware/**/*', 'src/logger/**/*'],
