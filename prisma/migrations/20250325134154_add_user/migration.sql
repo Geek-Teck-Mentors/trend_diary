de94bdc1f8490be01e7eb79f70697f61ea08a7b1
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-03-25 22:43:57 +0900

    fix: postgresの自動採番bigserialに変更

---
+++ prisma/migrations/20250325134154_add_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" BIGSERIAL NOT NULL,
+    "account_id" BIGINT NOT NULL,
+    "display_name" VARCHAR(1024) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- コメントをつける（手動）
+COMMENT ON COLUMN "users"."user_id" IS 'グローバルなID, クライアントには公開しない';
+COMMENT ON COLUMN "users"."account_id" IS 'グローバルなID, クライアントには公開しない';
+COMMENT ON COLUMN "users"."display_name" IS '表示名';
+COMMENT ON COLUMN "users"."created_at" IS '作成日時';
+COMMENT ON COLUMN "users"."updated_at" IS '更新日時';
