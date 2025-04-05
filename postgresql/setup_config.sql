f83b037b451407a81aa9bebdd103e68ebf7223cf
kouphasi <phasi.ok.phasi@gmail.com>
2025-04-05 19:02:15 +0900

    update: postresの設定をsqlファイルで実行する

---
+++ postgresql/setup_config.sql
@@ -0,0 +1,5 @@
+ALTER DATABASE trend_diary SET client_encoding to 'UTF8';
+ALTER DATABASE trend_diary SET TimeZone to 'Asia/Tokyo';
+ALTER DATABASE trend_diary SET datestyle to 'ISO, YMD';
+ALTER DATABASE trend_diary SET idle_in_transaction_session_timeout to '10min';
+ALTER DATABASE trend_diary SET log_min_duration_statement to '200ms';
