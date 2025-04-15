3432c0968dc080a5c7d1585e64dd4ed095b21259
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-04-15 23:35:53 +0900

    fix: supabaseで動作したもののみ残す

--- postgresql/setup_config.sql
+++ postgresql/setup_config.sql
@@ -1,5 +1,4 @@
-ALTER DATABASE trend_diary SET client_encoding to 'UTF8';
-ALTER DATABASE trend_diary SET TimeZone to 'Asia/Tokyo';
-ALTER DATABASE trend_diary SET datestyle to 'ISO, YMD';
-ALTER DATABASE trend_diary SET idle_in_transaction_session_timeout to '10min';
-ALTER DATABASE trend_diary SET log_min_duration_statement to '200ms';
+ALTER DATABASE postgres SET client_encoding to 'UTF8';
+ALTER DATABASE postgres SET TimeZone to 'Asia/Tokyo';
+ALTER DATABASE postgres SET datestyle to 'ISO, YMD';
+ALTER DATABASE postgres SET idle_in_transaction_session_timeout to '10min';
