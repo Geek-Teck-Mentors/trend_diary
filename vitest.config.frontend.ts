05c93034216d950f14ffc9adc1a28fa787b53ce2
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2025-04-17 00:57:54 +0900

    feat: frontend.configで定数化

--- vitest.config.frontend.ts
+++ vitest.config.frontend.ts
@@ -3,33 +3,36 @@
 import { defineConfig } from 'vite';
 import { coverageReporter, srcAlias } from './config';
 
+const testInclude = ['src/application/web/**/*.test.ts'];
+const testExclude = [
+  'src/application/api/**/*',
+  'src/application/middleware/**/*',
+  'src/domain/**/*',
+  'src/common/**/*',
+  'src/logger/**/*',
+];
+
+const coverageInclude = ['src/application/web/**/*', 'src/application/web/components/**/*'];
+const coverageExclude = [
+  ...testExclude,
+  'src/application/web/components/ui/**/*',
+  'src/application/web/lib/utils.ts',
+];
+
 export default defineConfig({
   resolve: {
     alias: [srcAlias],
   },
   test: {
     globals: true,
-    include: ['src/application/web/**/*.test.ts'],
-    exclude: [
-      'src/application/api/**/*',
-      'src/application/middleware/**/*',
-      'src/domain/**/*',
-      'src/common/**/*',
-      'src/logger/**/*',
-    ],
+    include: testInclude,
+    exclude: testExclude,
     // テストファイルがない場合にエラーになるため、テストファイルがない場合でも正常終了とする
     passWithNoTests: true,
     coverage: {
       reporter: coverageReporter,
-      include: ['src/application/web/**/*', 'src/application/web/components/**/*'],
-      exclude: [
-        'src/domain/**/*',
-        'src/common/**/*',
-        'src/logger/**/*',
-        'src/application/middleware/**/*',
-        'src/application/web/components/ui/**/*',
-        'src/application/web/lib/utils.ts',
-      ],
+      include: coverageInclude,
+      exclude: coverageExclude,
     },
   },
 });
