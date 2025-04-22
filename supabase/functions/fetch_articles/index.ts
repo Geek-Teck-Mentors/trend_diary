85dc58e12189daebf7f48ea99513eba70ac704f1
kouphasi <phasi.ok.phasi@gmail.com>
2025-04-22 23:59:02 +0900

    feat: Supabaseの設定ファイルとnpm設定を追加

---
+++ supabase/functions/fetch_articles/index.ts
@@ -0,0 +1,8 @@
+import { Hono } from 'jsr:@hono/hono'
+
+const functionName = 'hello-world'
+const app = new Hono().basePath(`/${functionName}`)
+
+app.get('/hello', (c) => c.text('Hello from hono-server!'))
+
+Deno.serve(app.fetch)
