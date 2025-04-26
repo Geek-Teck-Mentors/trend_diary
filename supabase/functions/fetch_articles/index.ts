f4fdbc09c24b0dc51329cdcf8844091c7a62724f
kouphasi <phasi.ok.phasi@gmail.com>
2025-04-26 22:20:16 +0900

    feat: fetch_articles関数を追加し、QiitaおよびZennのフィードを取得する機能を実装

--- supabase/functions/fetch_articles/index.ts
+++ supabase/functions/fetch_articles/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from 'jsr:@hono/hono'
+import { fetchQiitaFeed, fetchZennFeed } from './services/fetchFeed.ts'
 
-const functionName = 'hello-world'
+const functionName = 'fetch_articles'
 const app = new Hono().basePath(`/${functionName}`)
 
-app.get('/hello', (c) => c.text('Hello from hono-server!'))
+app.post('/', async (c) => {
+    const articles = await Promise.all([
+        fetchQiitaFeed(),
+        fetchZennFeed()
+    ]);
+    return c.json(articles);
+})
 
 Deno.serve(app.fetch)
