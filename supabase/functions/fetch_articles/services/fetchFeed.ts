4778cab969fb5d210b6f216272f7a1282160ac71
kouphasi <phasi.ok.phasi@gmail.com>
2025-04-26 22:20:05 +0900

    feat: fetchFeed関数を修正し、QiitaおよびZennのフィードを正しく取得して記事を一括作成する機能を実装

--- supabase/functions/fetch_articles/services/fetchFeed.ts
+++ supabase/functions/fetch_articles/services/fetchFeed.ts
@@ -1,30 +1,67 @@
-import { parseFeed } from "jsr:@mikaelporttila/rss@*";
+import Parser from 'npm:rss-parser@3.13.0';
+import { bulkCreateArticle, type BulkCreateArticlesParams } from "../repositories/articleRepository.ts";
 
-const QIITA_RUL =  "https://qiita.com/popular-items/feed.atom"
+const QIITA_URL =  "https://qiita.com/popular-items/feed.atom"
+type QiitaItem = {
+  title: string;
+  author: string;
+  content: string;
+  link: string;
+}
+
 const ZENN_URL =  "https://zenn.dev/feed";
+type ZennItem = {
+  title: string;
+  creator: string;
+  content: string;
+  link: string;
+}
 
-const fetchFeed = async (url: string) => {
-  const feedUrl = new URL(url);
+const fetchFeed = async <T>(url: string): Promise<T[]> => {
+  const parser = new Parser<{items: T[]}, T>();
 
-  const res = await fetch(feedUrl);
-
-  if (!res.ok) {
-    throw new Error(`Failed to fetch articles: ${res.statusText}`);
+  try {
+    const feed = await parser.parseURL(url);
+    return feed.items;
+  } catch (error) {
+    console.error("Error fetching feed:", error);
+    throw error;
   }
-
-  const xml = await res.text();
-
-  const feed = await parseFeed(xml);
-
-  return feed;
 };
 
 export const fetchQiitaFeed = async () => {
-  const feed = await fetchFeed(QIITA_RUL);
-  return feed;
+  const feedItems = await fetchFeed<QiitaItem>(QIITA_URL);
+
+  const params: BulkCreateArticlesParams = feedItems.map((item) => ({
+    media: "qiita",
+    title: item.title,
+    author: item.author,
+    description: item.content,
+    url: item.link,
+  }));
+
+  const { data: articles, error } = await bulkCreateArticle(params);
+  if (error) {
+    console.error("Error creating articles:", error);
+    throw error;
+  }
+  return articles;
 }
 
 export const fetchZennFeed = async () => {
-  const feed = await fetchFeed(ZENN_URL);
-  return feed;
+  const feedItems = await fetchFeed<ZennItem>(ZENN_URL);
+
+  const params: BulkCreateArticlesParams = feedItems.map((item) => ({
+    media: "zenn",
+    title: item.title,
+    author: item.creator,
+    description: item.content,
+    url: item.link,
+  }));
+  const { data: articles, error } = await bulkCreateArticle(params);
+  if (error) {
+    console.error("Error creating articles:", error);
+    throw error;
+  }
+  return articles;
 }
