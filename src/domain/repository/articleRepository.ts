4e4bfeae898e21558c4767f186c6f37587399111
kouphasi <phasi.ok.phasi@gmail.com>
2025-04-27 22:56:04 +0900

    feat: bulkCreateArticles関数を追加し、複数の記事を一度に作成できる機能を実装

--- src/domain/repository/articleRepository.ts
+++ src/domain/repository/articleRepository.ts
@@ -32,4 +32,35 @@ export default class ArticleRepositoryImpl implements ArticleRepository {
       newArticle.createdAt,
     );
   }
+
+  async bulkCreateArticles(
+    articles: Article[]
+  ): Promise<Article[]> {
+    await this.db.article.createMany({
+      data: articles.map((article) => ({
+        media: article.media,
+        title: article.title,
+        author: article.author,
+        description: article.description,
+        url: article.url,
+      })),
+    });
+
+    const newArticles = await this.db.article.findMany({
+      where: {
+        media: articles[0].media,
+        title: { in: articles.map((article) => article.title) },
+      },
+    });
+
+    return newArticles.map((newArticle) => new Article(
+      newArticle.articleId,
+      newArticle.media,
+      newArticle.title,
+      newArticle.author,
+      newArticle.description,
+      newArticle.url,
+      newArticle.createdAt,
+    ));
+  }
 }
