bee4f65c69da9f4ae487fd225235484f75c64b60
kouphasi <phasi.ok.phasi@gmail.com>
2025-04-29 01:07:43 +0900

    chore: format supabase functions code

--- supabase/functions/fetch_articles/index.ts
+++ supabase/functions/fetch_articles/index.ts
@@ -1,17 +1,17 @@
-import { Hono } from 'jsr:@hono/hono';
-import { fetchQiitaFeed, fetchZennFeed } from './fetchFeed.ts';
+import { Hono } from "jsr:@hono/hono";
+import { fetchQiitaFeed, fetchZennFeed } from "./fetchFeed.ts";
 
-const functionName = 'fetch_articles';
+const functionName = "fetch_articles";
 const app = new Hono().basePath(`/${functionName}`);
 
-app.post('/qiita', async (c) => {
+app.post("/qiita", async (c) => {
   await fetchQiitaFeed();
-  return c.json({status: 'ok', message: 'Qiita feed fetched successfully'});
+  return c.json({ status: "ok", message: "Qiita feed fetched successfully" });
 });
 
-app.post('/zenn', async (c) => {
+app.post("/zenn", async (c) => {
   await fetchZennFeed();
-  return c.json({status: 'ok', message: 'Zenn feed fetched successfully'});
+  return c.json({ status: "ok", message: "Zenn feed fetched successfully" });
 });
 
 Deno.serve(app.fetch);
