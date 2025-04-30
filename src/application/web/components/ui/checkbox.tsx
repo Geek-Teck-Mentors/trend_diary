eb95f378af6b34a9a3a6d0d96fb10ed1348726ee
YuukiHayashi0510 <hyuki749@gmail.com>
2025-05-01 00:24:34 +0900

    fix: shadcnのlibをui配下に移動

--- src/application/web/components/ui/checkbox.tsx
+++ src/application/web/components/ui/checkbox.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { CheckIcon } from 'lucide-react';
 
-import { cn } from '@/application/web/lib/utils';
+import { cn } from '@/application/web/components/ui/lib/utils';
 
 function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
   return (
