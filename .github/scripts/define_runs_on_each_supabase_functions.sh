1e268e791ec5b31c21f3a6cb523f0b9694d6257a
kouphasi <phasi.ok.phasi@gmail.com>
2025-05-10 22:48:20 +0900

    refactor: Supabase関数の実行スクリプトで除外関数の引数を修正

--- .github/scripts/define_runs_on_each_supabase_functions.sh
+++ .github/scripts/define_runs_on_each_supabase_functions.sh
@@ -2,10 +2,12 @@
 
 runs_on_each_supabase_functions() {
   local command=$1
-  local exclude_functions=${{ env.EXCLUDE_FUNCTIONS }}
+  local exclude_functions=$2
   for function_name in $(ls supabase/functions); do
     if [[ ! " ${exclude_functions[@]} " =~ " ${function_name} " ]]; then
       $command -c supabase/functions/${function_name}/deno.json
     fi
   done
 }
+
+export -f runs_on_each_supabase_functions
