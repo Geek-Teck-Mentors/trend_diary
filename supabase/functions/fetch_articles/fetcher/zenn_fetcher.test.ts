18c719c2d5d382ea5974cf9aae1aebca9e31ff9f
kouphasi <phasi.ok.phasi@gmail.com>
2025-05-22 00:14:33 +0900

    feat: qiita fetcherとzenn fetcherの結合テストの追加

---
+++ supabase/functions/fetch_articles/fetcher/zenn_fetcher.test.ts
@@ -0,0 +1,68 @@
+import { assert, assertEquals, assertThrowsAsync } from "https://deno.land/std@0.83.0/testing/asserts.ts";
+
+import {
+  assertSpyCallArgs,
+  assertSpyCalls,
+  stub,
+} from "https://deno.land/std@0.152.0/testing/mock.ts";
+
+import { ZennFetcher } from "./zenn_fetcher.ts";
+import Parser from "npm:rss-parser@3.13.0";
+import { ZennItem } from "../model/types.ts";
+import { MediaFetchError } from "../error.ts";
+
+// 正常系
+Deno.test("ParserのparseURLが呼び出されること", async () => {
+  const mockItems: ZennItem[] = [
+    { title: "Item 1", creator: "Author 1", content: "Content1", link: "https://example.com/item1" },
+    { title: "Item 2", creator: "Author 2", content: "Content2", link: "https://example.com/item2" },
+  ];
+  const mockFeed = { items: mockItems };
+  const parserStub = stub(Parser.prototype, "parseURL", () => {
+    return Promise.resolve(mockFeed);
+  });
+  const fetcher = new ZennFetcher();
+  await fetcher.fetch();
+  assertSpyCalls(parserStub, 1);
+});
+
+Deno.test("戻り値が、FeedItem[]であること", async () => {
+  const mockItems: ZennItem[] = [
+    { title: "Item 1", creator: "Author 1", content: "Content1", link: "https://example.com/item1" },
+    { title: "Item 2", creator: "Author 2", content: "Content2", link: "https://example.com/item2" },
+  ];
+  const mockFeed = { items: mockItems };
+  const parserStub = stub(Parser.prototype, "parseURL", () => {
+    return Promise.resolve(mockFeed);
+  });
+  const fetcher = new ZennFetcher();
+  const result = await fetcher.fetch();
+  assert(result.length > 0);
+  // FeedItem[]の型に合致することを確認
+  result.forEach((item) => {
+    assertEquals(Object.keys(item), ["title", "author", "description", "url"]);
+  });
+
+  parserStub.restore();
+});
+
+// 異常系
+Deno.test("ZennFetcher.fetch()が失敗すると、MediaFetchErrorが発生すること", async () => {
+  const parserStub = stub(Parser.prototype, "parseURL", () => {
+    return Promise.reject(new Error("Network error"));
+  }
+  );
+  const fetcher = new ZennFetcher();
+  await assertThrowsAsync(
+    async () => {
+      await fetcher.fetch();
+    }
+    ,
+    MediaFetchError,
+    "Failed to process feed items: Error: Network error"
+  );
+  assertSpyCalls(parserStub, 1);
+  assertSpyCallArgs(parserStub, 0, [fetcher.url]);
+  parserStub.restore();
+}
+);
