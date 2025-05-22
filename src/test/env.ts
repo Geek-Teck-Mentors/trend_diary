b9e4c56758d7b4fc79f3ad78a3f23a6563c5a05f
YuukiHayashi0510 <hyuki749@gmail.com>
2025-05-22 23:35:57 +0900

    fix: TEST_ENVはhono.envのbindings相当

--- src/test/env.ts
+++ src/test/env.ts
@@ -1,6 +1,7 @@
+import { Env } from '@/application/env';
+
 const TEST_ENV = {
   DATABASE_URL: process.env.DATABASE_URL ?? '',
-  SECRET_KEY: process.env.SECRET_KEY ?? 'test_secret_key',
-};
+} satisfies Env['Bindings'];
 
 export default TEST_ENV;
