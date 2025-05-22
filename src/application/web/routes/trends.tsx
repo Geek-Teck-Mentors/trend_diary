80618537cf5e0904cc06cfe3546128bfd9ad47b4
YuukiHayashi0510 <hyuki749@gmail.com>
2025-05-23 00:58:57 +0900

    feat: ログイン時のユーザ情報取得とリダイレクト処理の追加

--- src/application/web/routes/trends.tsx
+++ src/application/web/routes/trends.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { json, Outlet, useLoaderData } from '@remix-run/react';
+import { json, Outlet, redirect, useLoaderData } from '@remix-run/react';
 import AppSidebar from '../components/Sidebar';
 import { SidebarProvider } from '../components/ui/sidebar';
+import getApiClient from '@/infrastructure/api';
 
 export async function loader() {
+  const client = getApiClient(process.env.API_BASE_URL);
+  const res = await client.account.me.$get();
+
+  if (res.status >= 400 && res.status < 500) {
+    return redirect('/login');
+  }
+
   return json({
     ENV: {
       API_BASE_URL: process.env.API_BASE_URL,
