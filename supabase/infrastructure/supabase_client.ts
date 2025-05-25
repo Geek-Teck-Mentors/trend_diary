0995324ef1db36be08c830224658365eab26189a
kouphasi <phasi.ok.phasi@gmail.com>
2025-05-25 23:32:43 +0900

    feat: SUPABASE_URLとSUPABASE_ANON_KEYのデフォルト値を設定

--- supabase/infrastructure/supabase_client.ts
+++ supabase/infrastructure/supabase_client.ts
@@ -4,8 +4,9 @@ import { createClient } from "jsr:@supabase/supabase-js@2";
 import type { Database } from "./database.types.ts";
 
 export const rdbClient = createClient<Database>(
-  Deno.env.get("SUPABASE_URL") ?? "",
-  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+  // 引数はtruthyでなければならないので、仮のデフォルト値を設定
+  Deno.env.get("SUPABASE_URL") ?? "https://example.com",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? "anon-key",
 );
 
 export type RdbClient = typeof rdbClient;
