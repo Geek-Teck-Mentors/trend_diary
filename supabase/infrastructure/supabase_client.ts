ebe306128662ca555d8edaf3ad18c2e521bf4df5
kouphasi <phasi.ok.phasi@gmail.com>
2025-05-26 00:13:24 +0900

    feat: テスト実行時に環境ファイルを指定し、Supabaseのデフォルト値を削除

--- supabase/infrastructure/supabase_client.ts
+++ supabase/infrastructure/supabase_client.ts
@@ -4,9 +4,8 @@ import { createClient } from "jsr:@supabase/supabase-js@2";
 import type { Database } from "./database.types.ts";
 
 export const rdbClient = createClient<Database>(
-  // 引数はtruthyでなければならないので、仮のデフォルト値を設定
-  Deno.env.get("SUPABASE_URL") ?? "https://example.com",
-  Deno.env.get("SUPABASE_ANON_KEY") ?? "anon-key",
+  Deno.env.get("SUPABASE_URL") ?? "",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? "",
 );
 
 export type RdbClient = typeof rdbClient;
