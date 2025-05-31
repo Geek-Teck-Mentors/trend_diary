6db5302418d5ffeaf0a70f8d05a2294f4d4bf270
YuukiHayashi0510 <hyuki749@gmail.com>
2025-06-01 02:35:35 +0900

    refactor: APIクライアント用のURL変数名を変更し、明示性を向上

--- src/application/web/infrastructure/api.ts
+++ src/application/web/infrastructure/api.ts
@@ -1,13 +1,11 @@
 import getApiClient from '@/infrastructure/api';
 
-// クライアント専用コードであることを明示する
-let FOR_CLIENT_API_URL = '';
+let apiUrl = '';
 
 // useEffectとかの中で実行するか、このファイルが読み込まれるタイミングを制御する
 if (typeof document !== 'undefined') {
-  // documentを使う判定の方が安全なことが多いよ
-  FOR_CLIENT_API_URL = `${window.location.protocol}//${window.location.host}`;
+  apiUrl = `${window.location.protocol}//${window.location.host}`;
 }
 
-const getApiClientForClient = () => getApiClient(FOR_CLIENT_API_URL);
+const getApiClientForClient = () => getApiClient(apiUrl);
 export default getApiClientForClient;
