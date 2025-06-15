7cc8508fc771c659c60a96949cd715fc622bfb98
YuukiHayashi0510 <hyuki749@gmail.com>
2025-06-16 00:59:04 +0900

    refactor: ArticleCommandServiceおよびその実装を削除

--- src/domain/article/index.ts
+++ src/domain/article/index.ts
@@ -1,6 +1,5 @@
 export { default as Article } from './model/article';
 export { articleQuerySchema } from './schema/articleQuerySchema';
 export type { ArticleQueryParams } from './schema/articleQuerySchema';
-export type { ArticleCommandService } from './repository/articleCommandService';
 export type { ArticleQueryService } from './repository/articleQueryService';
 export { default as createArticleService } from './factory/articleServiceFactory';
--- src/domain/article/repository/articleCommandService.ts
+++
@@ -1,9 +0,0 @@
-import Article from '@/domain/article/model/article';
-import { ServerError } from '@/common/errors';
-import { AsyncResult } from '@/common/types/utility';
-
-export interface ArticleCommandService {
-  create(article: Omit<Article, 'articleId' | 'createdAt'>): AsyncResult<Article, ServerError>;
-  save(id: bigint, article: Partial<Article>): AsyncResult<Article, ServerError>;
-  delete(id: bigint): AsyncResult<void, ServerError>;
-}
