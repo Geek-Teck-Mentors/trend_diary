516836476c24b4f2c35b1bf20c6bb0a3e33a4aa9
kouphasi <phasi.ok.phasi@gmail.com>
2025-06-25 23:28:09 +0900

    fix: spinner circle

--- src/application/web/components/customized/spinner/spinner-09.tsx
+++ src/application/web/components/customized/spinner/spinner-09.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function SpinnerCircle3() {
-  <div className="w-7 h-7 border-[3px] border-secondary border-t-primary rounded-full animate-spin" />
+  return (
+    <div className="w-7 h-7 border-[3px] border-secondary border-t-primary rounded-full animate-spin" />
+  )
 }
 
 export default SpinnerCircle3;
