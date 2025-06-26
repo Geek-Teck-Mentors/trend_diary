ef70ff2f0e0814f051df5fb674ae1090e1bcf46d
kouphasi <phasi.ok.phasi@gmail.com>
2025-06-27 01:11:15 +0900

    feat: drawerをrouteページに移動

--- src/application/web/routes/_layout.trends/page.tsx
+++ src/application/web/routes/_layout.trends/page.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ArticleCard from './components/ArticleCard';
-import ArticleDrawer from './components/ArticleDrawer';
 import { Article } from './types';
 import SpinnerCircle3 from '../../components/customized/spinner/spinner-09';
 
 type Props = {
   articles: Article[];
   date: Date;
-  selectedArticle: Article | null;
-  isDrawerOpen: boolean;
   openDrawer: (article: Article) => void;
-  closeDrawer: () => void;
   isLoading: boolean;
   observerTargetRef: React.RefObject<HTMLDivElement>;
 };
@@ -18,37 +14,31 @@ type Props = {
 export default function TrendsPage({
   articles,
   date,
-  selectedArticle,
-  isDrawerOpen,
   openDrawer,
-  closeDrawer,
   isLoading,
   observerTargetRef,
 }: Props) {
   const handleCardClick = (article: Article) => {
     openDrawer(article);
-  }
+  };
   return (
-    <>
-      <div className='relative min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 p-6'>
-        <h1 className='pb-4 text-xl italic'>- {date.toLocaleDateString('ja-JP')} -</h1>
-        <div className='flex flex-wrap gap-4'>
-          {articles.length === 0 ? (
-            <p className='text-gray-500'>記事がありません</p>
-          ) : (
-            articles.map((article) => (
-              <ArticleCard key={article.articleId} article={article} onCardClick={handleCardClick} />
-            ))
-          )}
-        </div>
-        <div ref={observerTargetRef} className='h-4 w-full' />
-        {isLoading && (
-          <div className='bg-opacity-75 absolute inset-0 flex items-center justify-center bg-gray-50'>
-            <SpinnerCircle3 />
-          </div>
+    <div className='relative min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 p-6'>
+      <h1 className='pb-4 text-xl italic'>- {date.toLocaleDateString('ja-JP')} -</h1>
+      <div className='flex flex-wrap gap-4'>
+        {articles.length === 0 ? (
+          <p className='text-gray-500'>記事がありません</p>
+        ) : (
+          articles.map((article) => (
+            <ArticleCard key={article.articleId} article={article} onCardClick={handleCardClick} />
+          ))
         )}
       </div>
-      <ArticleDrawer article={selectedArticle!} isOpen={isDrawerOpen} onClose={closeDrawer} />
-    </>
+      <div ref={observerTargetRef} className='h-4 w-full' />
+      {isLoading && (
+        <div className='bg-opacity-75 absolute inset-0 flex items-center justify-center bg-gray-50'>
+          <SpinnerCircle3 />
+        </div>
+      )}
+    </div>
   );
 }
--- src/application/web/routes/_layout.trends/route.tsx
+++ src/application/web/routes/_layout.trends/route.tsx
@@ -3,39 +3,31 @@ import type { MetaFunction } from '@remix-run/cloudflare';
 import TrendsPage from './page';
 import useTrends from './useTrends';
 import useDrawerState from './useDrawerState';
+import ArticleDrawer from './components/ArticleDrawer';
 
 export const meta: MetaFunction = () => [{ title: 'トレンド一覧 | TrendDiary' }];
 
 export default function Trends() {
-  const {
-    isDrawerOpen,
-    selectedArticle,
-    openDrawer,
-    closeDrawer,
-  } = useDrawerState();
+  const { isDrawerOpen, selectedArticle, openDrawer, closeDrawer } = useDrawerState();
 
-  const {
-    articles,
-    fetchArticles,
-    date,
-    isLoading,
-    observerTargetRef,
-  } = useTrends();
+  const { articles, fetchArticles, date, isLoading, observerTargetRef } = useTrends();
 
   useEffect(() => {
     fetchArticles();
   }, []);
 
   return (
-    <TrendsPage
-      articles={articles}
-      selectedArticle={selectedArticle}
-      date={date}
-      isDrawerOpen={isDrawerOpen}
-      openDrawer={openDrawer}
-      closeDrawer={closeDrawer}
-      isLoading={isLoading}
-      observerTargetRef={observerTargetRef}
-    />
+    <>
+      <TrendsPage
+        articles={articles}
+        date={date}
+        openDrawer={openDrawer}
+        isLoading={isLoading}
+        observerTargetRef={observerTargetRef}
+      />
+      {selectedArticle && (
+        <ArticleDrawer article={selectedArticle!} isOpen={isDrawerOpen} onClose={closeDrawer} />
+      )}
+    </>
   );
 }
