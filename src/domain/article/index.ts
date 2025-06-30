bedff9520e1a33dcf8336a4d26f2704db7f2b359
YuukiHayashi0510 <hyuki749@gmail.com>
2025-06-30 17:50:07 +0900

    fix: 不要なexport

--- src/domain/article/index.ts
+++ src/domain/article/index.ts
@@ -1,8 +1,6 @@
 export { default as createArticleService } from './factory/articleServiceFactory'
 export { default as Article } from './model/article'
 export { default as ReadHistory } from './model/readHistory'
-export type { ArticleCommandService } from './repository/articleCommandService'
-export type { ArticleQueryService } from './repository/articleQueryService'
 export type { ArticleQueryParams } from './schema/articleQuerySchema'
 export { articleQuerySchema } from './schema/articleQuerySchema'
 export type {
