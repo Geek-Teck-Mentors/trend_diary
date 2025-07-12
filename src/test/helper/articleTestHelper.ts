21b67d3d44621fbf668d736e688b2b906fbc0d4f
kouphasi <phasi.ok.phasi@gmail.com>
2025-07-12 18:07:55 +0900

    fix: createArticleを削除していて、一部テストが通らなかったのを修正

--- src/test/helper/articleTestHelper.ts
+++ src/test/helper/articleTestHelper.ts
@@ -7,8 +7,20 @@ process.env.NODE_ENV = 'test'
 class ArticleTestHelper {
   private rdb = getRdbClient(TEST_ENV.DATABASE_URL)
 
+  async createArticle() {
+    return await this.rdb.article.create({
+      data: {
+        media: faker.helpers.arrayElement(['qiita', 'zenn']),
+        title: faker.lorem.sentence(),
+        author: faker.person.fullName(),
+        description: faker.lorem.paragraph(),
+        url: faker.internet.url(),
+      }
+    })
+  }
+
   async createArticles(
-    params:
+    params?:
       | {
           media: 'qiita' | 'zenn'
           title: string
@@ -16,7 +28,6 @@ class ArticleTestHelper {
           description: string
           url: string
         }[]
-      | null,
   ) {
     const data = params ?? [
       {
