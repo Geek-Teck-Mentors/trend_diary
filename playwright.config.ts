5142545c9bdfe356d204c7b40963b5b0bb66cdd7
kouphasi <phasi.ok.phasi@gmail.com>
2025-07-15 22:38:30 +0900

    chore: playwright.configの設定を戻す

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,15 +14,13 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './src/test/e2e',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* timeout */
-  timeout: process.env.CI ? 30000 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
