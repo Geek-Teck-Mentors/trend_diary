309e6a6d90bf59c2dd29bea8825b72270de48ecb
YuukiHayashi0510 <hyuki749@gmail.com>
2025-07-26 00:43:13 +0900

    chore: biomeが自動で効く設定にする

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,20 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(ts|tsx)"],
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
   addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {
       builder: {
-        viteConfigPath: "vitest/config.storybook.ts",
+        viteConfigPath: 'vitest/config.storybook.ts',
       },
     },
   },
-};
-export default config;
+}
+export default config
