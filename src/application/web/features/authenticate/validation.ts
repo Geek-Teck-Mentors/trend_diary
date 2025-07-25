0c1c0b2233348cdff32af013a7fbbc09bff8e866
YuukiHayashi0510 <hyuki749@gmail.com>
2025-07-26 01:05:43 +0900

    refactor: バリデーション結果の処理を共通化

--- src/application/web/features/authenticate/validation.ts
+++ src/application/web/features/authenticate/validation.ts
@@ -1,5 +1,6 @@
 import { accountSchema } from '@/domain/account'
 import { z } from 'zod'
+import { newValidationError, newValidationSuccess, ValidationResult } from '../common/validation'
 
 export type AuthenticateErrors = {
   email?: string[]
@@ -8,17 +9,7 @@ export type AuthenticateErrors = {
 
 export type AuthenticateFormData = Pick<z.infer<typeof accountSchema>, 'email' | 'password'>
 
-type ValidationResult =
-  | {
-      isValid: true
-      data: AuthenticateFormData
-    }
-  | {
-      isValid: false
-      errors: AuthenticateErrors
-    }
-
-export function validateAuthenticateForm(formData: FormData): ValidationResult {
+export function validateAuthenticateForm(formData: FormData): ValidationResult<AuthenticateFormData, AuthenticateErrors> {
   const email = formData.get('email') as string
   const password = formData.get('password') as string
 
@@ -28,14 +19,8 @@ export function validateAuthenticateForm(formData: FormData): ValidationResult {
   })
 
   if (!result.success) {
-    return {
-      isValid: false,
-      errors: result.error.flatten().fieldErrors,
-    }
+    return newValidationError<AuthenticateErrors>(result.error.flatten().fieldErrors as AuthenticateErrors)
   }
 
-  return {
-    isValid: true,
-    data: { email: result.data.email, password: result.data.password },
-  }
+  return newValidationSuccess<AuthenticateFormData>({ email: result.data.email, password: result.data.password })
 }
