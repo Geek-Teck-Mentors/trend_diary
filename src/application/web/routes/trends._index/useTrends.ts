f30ccdbf8df5bdcfb7c1339d38c8063bca69e043
kouphasi <phasi.ok.phasi@gmail.com>
2025-07-27 16:07:04 +0900

    feat: ヘッダーの日付が再レンダリング時でも変動するように

--- src/application/web/routes/trends._index/useTrends.ts
+++ src/application/web/routes/trends._index/useTrends.ts
@@ -82,7 +82,7 @@ export default function useTrends() {
   // INFO: 初回読み込み時に今日の日付で記事を取得
   useEffect(() => {
     fetchArticles({ date })
-  }, [])
+  }, [date])
 
   const handleNextPage = useCallback(async () => {
     if (cursor.next) {
