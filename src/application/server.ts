6d2c396520272827cdb89621bd1da69bf1794bb9
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-03 13:19:24 +0900

    chore: hot reload

--- src/application/server.ts
+++ src/application/server.ts
@@ -4,6 +4,7 @@ import apiApp from './api/route'
 import { Env } from './env'
 import errorHandler from './middleware/errorHandler'
 import loggerMiddleware from './middleware/requestLogger'
+import { AppLoadContext, createRequestHandler } from 'react-router'
 
 const app = new Hono<Env>()
 
@@ -13,18 +14,19 @@ app.onError(errorHandler)
 app.use('/api', timeout(5000))
 app.route('/api', apiApp)
 
-// if (process.env.NODE_ENV === 'development')
-//   app.all('*', async (c) => {
-//     // remixのビルド結果をhonoにうまく繋ぎこむために使う virtual import
-//     // @ts-expect-error it's not typed
-//     const build = await import('virtual:@react-router/server-build')
-//     const handler = createRequestHandler(build, 'development')
-//     const remixContext = {
-//       cloudflare: {
-//         env: c.env,
-//       },
-//     } as unknown as AppLoadContext
-//     return handler(c.req.raw, remixContext)
-//   })
+// hotReload用
+if (process.env.NODE_ENV === 'development')
+  app.all('*', async (c) => {
+    // ビルド結果をhonoにうまく繋ぎこむために使う virtual import
+    // @ts-expect-error it's not typed
+    const build = await import('virtual:react-router/server-build')
+    const handler = createRequestHandler(build, 'development')
+    const remixContext = {
+      cloudflare: {
+        env: c.env,
+      },
+    } as unknown as AppLoadContext
+    return handler(c.req.raw, remixContext)
+  })
 
 export default app
