bf6ade47f5155396b5b0fc4a499067acb1b4bd11
kouphasi <phasi.ok.phasi@gmail.com>
2025-08-11 20:07:34 +0900

    refactor: formatted by biome

--- vitest/config.storybook.ts
+++ vitest/config.storybook.ts
@@ -27,10 +27,7 @@ export default defineConfig(({ mode }) => {
           'src/application/web/components/**/*.tsx',
           'src/application/web/features/**/*.tsx',
         ],
-        exclude: [
-          'src/application/web/components/ui',
-          'src/application/web/components/customized',
-        ],
+        exclude: ['src/application/web/components/ui', 'src/application/web/components/customized'],
         thresholds: {
           statements: 80,
           branches: 80,
