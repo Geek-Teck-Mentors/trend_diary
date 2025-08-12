6e99cdc94e105c15fa4b67286571049fd69fada6
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-12 22:13:58 +0900

    feat: テスト環境でユーザー機能を常に有効にするように設定を変更

--- src/test/env.ts
+++ src/test/env.ts
@@ -3,7 +3,7 @@ import { Env } from '@/application/env'
 const TEST_ENV = {
   DATABASE_URL: process.env.DATABASE_URL ?? 'postgresql://postgres:password@localhost:5432/test',
   DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL ?? '',
-  FEATURE_USER_ENABLED: process.env.FEATURE_USER_ENABLED ?? 'false',
+  FEATURE_USER_ENABLED: process.env.FEATURE_USER_ENABLED ?? 'true', // テストでは常に有効にする
 } satisfies Env['Bindings']
 
 export default TEST_ENV
