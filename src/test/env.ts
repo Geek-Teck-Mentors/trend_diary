05af92c0695572d21c9e0f22c637aea6251e8e0d
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-12 22:07:46 +0900

    feat: 環境設定にユーザー機能のフラグを追加

--- src/test/env.ts
+++ src/test/env.ts
@@ -3,6 +3,7 @@ import { Env } from '@/application/env'
 const TEST_ENV = {
   DATABASE_URL: process.env.DATABASE_URL ?? 'postgresql://postgres:password@localhost:5432/test',
   DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL ?? '',
+  FEATURE_USER_ENABLED: process.env.FEATURE_USER_ENABLED ?? 'false',
 } satisfies Env['Bindings']
 
 export default TEST_ENV
