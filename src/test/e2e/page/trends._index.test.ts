35ce171c146b6499d31de1d880b621a19c792e70
kouphasi <phasi.ok.phasi@gmail.com>
2025-08-16 17:28:22 +0900

    refactor: テストコードの調整

--- src/test/e2e/page/trends._index.test.ts
+++ src/test/e2e/page/trends._index.test.ts
@@ -91,10 +91,7 @@ test.describe('記事一覧ページ', () => {
       await drawerLink.click()
 
       // 5. 新しいタブでそのリンクのページに遷移する
-      const [newPage] = await Promise.all([
-        page.context().waitForEvent('page'),
-        page.waitForLoadState('networkidle'),
-      ])
+      const newPage = await page.context().waitForEvent('page');
       await expect(newPage).toHaveURL(ARTICLE_URL)
     })
   })
