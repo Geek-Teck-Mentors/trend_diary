7fc3696d6dd246176c4552a3a97826c8b76c7e10
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-19 17:21:02 +0900

    feat: ログインおよびサインアップルートを追加

--- src/application/web/routes.ts
+++ src/application/web/routes.ts
@@ -2,9 +2,13 @@ import { index, layout, type RouteConfig, route } from '@react-router/dev/routes
 
 export default [
   index('./routes/_index.tsx'),
-  // TODO: アカウントが必要な機能ができてから有効化する
-  // route('/login', './routes/login/route.tsx'),
-  // route('/signup', './routes/signup/route.tsx'),
-
   layout('./routes/trends.tsx', [route('/trends', './routes/trends._index/route.tsx')]),
+
+  route('/login', './routes/login/route.tsx'),
+  route('/signup', './routes/signup/route.tsx'),
+
+  layout('./routes/admin._layout.tsx', [
+    route('/admin', './routes/admin._index.tsx'),
+    route('/admin/users', './routes/admin.users/route.tsx'),
+  ]),
 ] satisfies RouteConfig
