8ac4a4a25b994340cfedc3950e8d6d584817d397
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-20 17:11:59 +0900

    fix: displayNameの型をNullable<string>に変更し、adminUserIdの初期値をnullに設定

--- src/domain/user/model/activeUser.ts
+++ src/domain/user/model/activeUser.ts
@@ -1,14 +1,16 @@
+import { Nullable } from "@/common/types/utility";
+
 export default class ActiveUser {
   constructor(
     public activeUserId: bigint,
     public userId: bigint,
     public email: string,
     public password: string,
-    public displayName?: string | null,
+    public displayName?: Nullable<string>,
     public lastLogin?: Date,
     public readonly createdAt: Date = new Date(),
     public updatedAt: Date = new Date(),
-    public readonly isAdmin: boolean = false,
+    public readonly adminUserId: Nullable<number> = null,
   ) {}
 
   recordLogin(): void {
