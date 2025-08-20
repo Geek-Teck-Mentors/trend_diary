5038cfcb6bb230a9051b14da9e2cd1c1e913a53f
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-20 21:21:19 +0900

    factoryを浅くする

--- src/domain/article/index.ts
+++ src/domain/article/index.ts
@@ -1,4 +1,14 @@
-export { default as createArticleService } from './factory/articleServiceFactory'
+import { RdbClient } from '@/infrastructure/rdb'
+import ArticleCommandServiceImpl from './infrastructure/articleCommandServiceImpl'
+import ArticleQueryServiceImpl from './infrastructure/articleQueryServiceImpl'
+import ArticleService from './service/articleService'
+
+export function createArticleService(db: RdbClient): ArticleService {
+  const articleQueryService = new ArticleQueryServiceImpl(db)
+  const articleCommandService = new ArticleCommandServiceImpl(db)
+  return new ArticleService(articleQueryService, articleCommandService)
+}
+
 export type { ArticleQueryParams } from './schema/articleQuerySchema'
 export { articleQuerySchema } from './schema/articleQuerySchema'
 export type { Article } from './schema/articleSchema'
