448fb60406bd580562397dd2c4ac64f731117c32
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-23 22:38:17 +0900

    feat: LinkAsButtonコンポーネントの型定義を改善し、内部リンクと外部リンクのプロパティを明確に分離

--- src/application/web/components/linkAsButton/index.tsx
+++ src/application/web/components/linkAsButton/index.tsx
@@ -1,14 +1,32 @@
 import { PropsWithChildren } from 'react'
 import { Link } from 'react-router'
 import { Button } from '../ui/button'
+import { InternalPath } from '../../routes'
 
-type Props = {
-  to: string
+interface BaseProps {
   className?: string
-  external?: boolean
 }
 
-// Linkのperf参考: https://zenn.dev/atusi/articles/3e37d4d54736fa#link
+interface InternalProps extends BaseProps {
+  to: InternalPath
+  external?: false
+}
+
+interface ExternalProps extends BaseProps {
+  to: `https://${string}`
+  external: true
+}
+
+type Props = InternalProps | ExternalProps
+
+/**
+ * LinkAsButton
+ * @description 内部リンクの際はReact RouterのLink、外部リンクの際はaタグとして振る舞うボタンコンポーネント
+ * @param to InternalPath | `https://${string}`
+ * @param external false | true
+ * @param className Optional class name for styling
+ * @link Linkのperf参考: https://zenn.dev/atusi/articles/3e37d4d54736fa#link
+ */
 export default function LinkAsButton({
   to,
   children,
