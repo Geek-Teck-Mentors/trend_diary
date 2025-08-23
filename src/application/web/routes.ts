d9555fd056815d27b2f22fe7f08d9e395f71782d
YuukiHayashi0510 <hyuki749@gmail.com>
2025-08-23 21:17:34 +0900

    refactor: ルーティングを構造化し、ReactRouterを意識せずに設定できるようにする

--- src/application/web/routes.ts
+++ src/application/web/routes.ts
@@ -1,14 +1,59 @@
-import { index, layout, type RouteConfig, route } from '@react-router/dev/routes'
+import {
+  index,
+  layout,
+  prefix,
+  type RouteConfig,
+  RouteConfigEntry,
+  route,
+} from '@react-router/dev/routes'
 
-export default [
-  index('./routes/_index.tsx'),
-  layout('./routes/trends.tsx', [route('/trends', './routes/trends._index/route.tsx')]),
+const PATH_INDEX = '/'
 
-  route('/login', './routes/login/route.tsx'),
-  route('/signup', './routes/signup/route.tsx'),
+interface GroupRoute {
+  prefix: string
+  layout?: string
+  routes: Route[]
+}
 
-  layout('./routes/admin._layout.tsx', [
-    route('/admin', './routes/admin._index.tsx'),
-    route('/admin/users', './routes/admin.users/route.tsx'),
-  ]),
-] satisfies RouteConfig
+interface Route {
+  path: string
+  file: `./routes/${string}.tsx`
+}
+
+const baseGroup: GroupRoute = {
+  prefix: '',
+  routes: [
+    { path: PATH_INDEX, file: './routes/_index.tsx' },
+    { path: '/login', file: './routes/login/route.tsx' },
+    { path: '/signup', file: './routes/signup/route.tsx' },
+  ],
+}
+
+const trendGroup: GroupRoute = {
+  prefix: 'trends',
+  layout: './routes/trends.tsx',
+  routes: [{ path: PATH_INDEX, file: './routes/trends._index/route.tsx' }],
+}
+
+const adminGroup: GroupRoute = {
+  prefix: 'admin',
+  layout: './routes/admin._layout.tsx',
+  routes: [
+    { path: PATH_INDEX, file: './routes/admin._index.tsx' },
+    { path: '/users', file: './routes/admin.users/route.tsx' },
+  ],
+}
+
+function buildGroupRoute(group: GroupRoute): RouteConfigEntry[] {
+  const routes = group.routes.map((value) =>
+    value.path === PATH_INDEX ? index(value.file) : route(value.path, value.file),
+  )
+  const layoutRoutes = group.layout ? [layout(group.layout, routes)] : routes
+
+  return group.prefix ? prefix(group.prefix, layoutRoutes) : layoutRoutes
+}
+
+const groupRoutes: GroupRoute[] = [baseGroup, trendGroup, adminGroup]
+
+const routing = groupRoutes.flatMap(buildGroupRoute)
+export default routing satisfies RouteConfig
