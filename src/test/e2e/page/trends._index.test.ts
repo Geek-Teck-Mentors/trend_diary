9e534a1615ad82f84e882390146a1c3d2c29e906
kouphasi <phasi.ok.phasi@gmail.com>
2025-08-24 19:07:04 +0900

    feat: e2eでloading spinnerが消えるまで待機するように

--- src/test/e2e/page/trends._index.test.ts
+++ src/test/e2e/page/trends._index.test.ts
@@ -23,6 +23,8 @@ test.describe('記事一覧ページ', () => {
 
   test.describe('記事がない場合', () => {
     test('記事がないと表示される', async ({ page }) => {
+      // loadingスピナーが消えるのを待機
+      await page.getByRole('status').waitFor({ state: 'detached', timeout: 10000 })
       console.log("aa",await page.locator("[data-slot='card']").isVisible())
       console.log("bb",await page.locator("[data-slot='card']").first().isVisible())
       console.log("cc",await page.getByText('記事がありません').isVisible())
