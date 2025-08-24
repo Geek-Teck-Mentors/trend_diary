8618b2412c60c2faa765455569107711bfefe70c
kouphasi <phasi.ok.phasi@gmail.com>
2025-08-24 15:37:58 +0900

    Revert "feat: playwright configを調整"
    This reverts commit 6a51c7276d09f5ab25547ee63342f2a0d54575fd.

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm start',
     url: 'http://localhost:5173',
     // INFO: CIに合わせる
     reuseExistingServer: false,
