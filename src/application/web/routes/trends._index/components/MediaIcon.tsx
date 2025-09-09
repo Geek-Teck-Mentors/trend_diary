b1a912d6596fada843006444baac5e3bb763c80e
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-09 23:39:27 +0900

    refactor: MediaIconの定数の形式の整理

--- src/application/web/routes/trends._index/components/MediaIcon.tsx
+++ src/application/web/routes/trends._index/components/MediaIcon.tsx
@@ -1,19 +1,21 @@
 type MediaType = 'qiita' | 'zenn'
-const mediaIconImages = {
-  qiita: '/images/qiita-icon.png',
-  zenn: '/images/zenn-icon.svg',
-} as const
-
-const mediaAltText = {
-  qiita: 'qiita icon',
-  zenn: 'zenn icon',
-} as const
+const mediaAttributes = {
+  qiita: {
+    iconImage: '/images/qiita-icon.png',
+    altText: 'qiita icon',
+  },
+  zenn: {
+    iconImage: '/images/zenn-icon.svg',
+    altText: 'zenn icon',
+  },
+} as const;
 
 export default function MediaTag({ media }: { media: MediaType }) {
+  const { iconImage, altText } = mediaAttributes[media]
   return (
     <img
-      src={mediaIconImages[media]}
-      alt={mediaAltText[media]}
+      src={iconImage}
+      alt={altText}
       className='inline-block w-4 h-4 align-middle mr-1.5 mb-1'
       data-slot='media-icon'
     />
