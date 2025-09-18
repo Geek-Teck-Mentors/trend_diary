434698d9a2053ef4368edb7778192724b8887348
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-18 23:06:32 +0900

    feat: LoadingSpinnerの存在確認もstepで行うように

--- src/application/web/components/LoadingSpinner/LoadingSpinner.stories.tsx
+++ src/application/web/components/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -14,18 +14,21 @@ type Story = StoryObj<typeof LoadingSpinner>
 
 export const Default: Story = {
   play: async ({ canvas, step }) => {
-    // LoadingSpinnerのコンテナ要素をroleで取得
-    const container = canvas.getByRole('status', { name: 'Loading...' })
-    await expect(container).toBeInTheDocument()
-    await expect(container).toBeVisible()
+    await step('LoadingSpinnerコンポーネントが表示されることを確認', async () => {
+      const container = canvas.getByRole('status', { name: 'Loading...' })
+      await expect(container).toBeInTheDocument()
+      await expect(container).toBeVisible()
+    })
 
     await step('スピナーが表示されることを確認', async () => {
+      const container = canvas.getByRole('status', { name: 'Loading...' })
       const spinner = container.querySelector('div > div')
       await expect(spinner).toBeInTheDocument()
       await expect(spinner).toBeVisible()
     })
 
     await step('スピナーが回転アニメーションしていることを確認', async () => {
+      const container = canvas.getByRole('status', { name: 'Loading...' })
       const spinner = container.querySelector('div > div')
       const spinnerStyle = window.getComputedStyle(spinner as Element)
       await expect(spinnerStyle.animation).toContain('spin')
