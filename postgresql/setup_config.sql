d4fa0dc285f7756ce2f54d6f92a62377028cccdb
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-24 00:11:11 +0900

    fix: e2eテストでmigrationが失敗するのを修正

--- postgresql/setup_config.sql
+++ postgresql/setup_config.sql
@@ -2,3 +2,4 @@ ALTER DATABASE postgres SET client_encoding to 'UTF8';
 ALTER DATABASE postgres SET TimeZone to 'Asia/Tokyo';
 ALTER DATABASE postgres SET datestyle to 'ISO, YMD';
 ALTER DATABASE postgres SET idle_in_transaction_session_timeout to '10min';
+CREATE ROLE anon;
