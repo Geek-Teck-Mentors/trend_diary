7a2b8640d4ae8040b75f2cd92eba224d37c20e1d
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-25 00:18:05 +0900

    feat: migration側でroleを作成するように

--- postgresql/setup_config.sql
+++ postgresql/setup_config.sql
@@ -2,4 +2,3 @@ ALTER DATABASE postgres SET client_encoding to 'UTF8';
 ALTER DATABASE postgres SET TimeZone to 'Asia/Tokyo';
 ALTER DATABASE postgres SET datestyle to 'ISO, YMD';
 ALTER DATABASE postgres SET idle_in_transaction_session_timeout to '10min';
-CREATE ROLE anon; -- Supabase固有のRoleを作成
