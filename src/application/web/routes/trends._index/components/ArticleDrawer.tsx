608c320e8ede36e4abd9ac6af0eb4dfdb1f4c6f0
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-25 23:11:12 +0900

    refactor: data-vaul-no-dragを使用してドラッグを阻止するように変更

--- src/application/web/routes/trends._index/components/ArticleDrawer.tsx
+++ src/application/web/routes/trends._index/components/ArticleDrawer.tsx
@@ -37,10 +37,9 @@ export default function ArticleDrawer({ article, isOpen, onClose }: Props) {
           </DrawerClose>
         </DrawerHeader>
 
-        {/* Drawer内では文字選択とドラッグしてDrawerを閉じるアクションがバッティングするため、onPointerDownを上書きし、ドラッグしてDrawerを閉じれないように */}
         <div
           className='flex-1 overflow-y-auto px-4 select-text'
-          onPointerDown={(e) => e.stopPropagation()}
+          data-vaul-no-drag={false}
         >
           <DrawerTitle className='mb-4 text-xl leading-relaxed font-bold text-gray-900'>
             {article.title}
