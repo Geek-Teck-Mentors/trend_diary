d0b0fa42a9bef408c503fe83e5ae3e8a96022333
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-26 00:22:58 +0900

    refactor: zodを用いて日付変換メソッドのvalidationを実装

--- src/common/locale/date.ts
+++ src/common/locale/date.ts
@@ -1,5 +1,11 @@
+import { z } from 'zod';
+
+const dateSchema = z.union([z.string().datetime(), z.date()]);
+
 export const toJaDateString = (value: string | Date): string => {
-  const date = new Date(value)
-  if (Number.isNaN(date.getTime())) return ''
-  return date.toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' })
-}
+  const parseResult = dateSchema.safeParse(value);
+  if (!parseResult.success) return '';
+
+  const date = new Date(value);
+  return date.toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' });
+};
