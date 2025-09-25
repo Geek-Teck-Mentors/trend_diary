319da18c65ae692715359eede056bd0f44525c02
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-25 23:41:55 +0900

    refactor: fixed by biome

--- src/common/locale/date.ts
+++ src/common/locale/date.ts
@@ -1,5 +1,5 @@
 export const toJaDateString = (value: string | Date): string => {
-  const date = new Date(value);
-  if (isNaN(date.getTime())) return '';
-  return date.toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' });
+  const date = new Date(value)
+  if (Number.isNaN(date.getTime())) return ''
+  return date.toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' })
 }
