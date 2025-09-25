0227b633b170f0363b3cec0eb66a9c22ac464fd6
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-26 00:23:51 +0900

    style: formatted by biome

--- src/common/locale/date.ts
+++ src/common/locale/date.ts
@@ -1,11 +1,11 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
-const dateSchema = z.union([z.string().datetime(), z.date()]);
+const dateSchema = z.union([z.string().datetime(), z.date()])
 
 export const toJaDateString = (value: string | Date): string => {
-  const parseResult = dateSchema.safeParse(value);
-  if (!parseResult.success) return '';
+  const parseResult = dateSchema.safeParse(value)
+  if (!parseResult.success) return ''
 
-  const date = new Date(value);
-  return date.toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' });
-};
+  const date = new Date(value)
+  return date.toLocaleDateString('ja-JP', { timeZone: 'Asia/Tokyo' })
+}
