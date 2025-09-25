63fc731cec281d2fe5de005739782868260b3491
kouphasi <phasi.ok.phasi@gmail.com>
2025-09-25 23:11:34 +0900

    refactor: formatted by biome

--- src/application/web/routes/trends._index/components/ArticleDrawer.tsx
+++ src/application/web/routes/trends._index/components/ArticleDrawer.tsx
@@ -37,10 +37,7 @@ export default function ArticleDrawer({ article, isOpen, onClose }: Props) {
           </DrawerClose>
         </DrawerHeader>
 
-        <div
-          className='flex-1 overflow-y-auto px-4 select-text'
-          data-vaul-no-drag={false}
-        >
+        <div className='flex-1 overflow-y-auto px-4 select-text' data-vaul-no-drag={false}>
           <DrawerTitle className='mb-4 text-xl leading-relaxed font-bold text-gray-900'>
             {article.title}
           </DrawerTitle>
