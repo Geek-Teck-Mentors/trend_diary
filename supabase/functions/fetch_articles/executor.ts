360070cd3609975db2e1c67b6907b7142734e855
kouphasi <phasi.ok.phasi@gmail.com>
2025-10-19 22:24:43 +0900

    feat: functionsでDB上での重複を取り除いて保存するように

--- supabase/functions/fetch_articles/executor.ts
+++ supabase/functions/fetch_articles/executor.ts
@@ -1,5 +1,6 @@
 import { ArticleFetcher, ArticleRepository } from "./model/interface.ts";
 import { ArticleInput } from "./model/model.ts";
+import { FeedItem } from "./model/types.ts";
 
 export class Executor {
   constructor(
@@ -10,24 +11,38 @@ export class Executor {
 
   async do() {
     console.log("Executing fetcher for media:", this.media);
-    const items = await this.fetcher.fetch();
-    if (items.length === 0) {
+    const fetchedItems = await this.fetcher.fetch();
+    if (fetchedItems.length === 0) {
       return { message: "no items" };
     }
 
+    const existingArticles = await this.fetchExistingArticles(fetchedItems);
+    const existingUrls = new Set(existingArticles.map((article) => article.url));
+
+    const items = fetchedItems.filter((item) => !existingUrls.has(item.url));
+
     console.log("Inserting items into repository:", items.length);
-    const articles = await this.repository.bulkCreateArticle(
-      items.map((item) => ({
-        media: this.media,
-        title: item.title,
-        author: item.author,
-        description: item.description,
-        url: item.url,
-      } satisfies ArticleInput)),
-    );
+    const articles = await this.bulkCreateArticles(items);
 
     return {
       message: `Articles fetched successfully: ${articles.length}`,
     };
+  }
+
+  private async fetchExistingArticles(items: FeedItem[]) {
+    const urls = items.map((item) => item.url);
+    return await this.repository.fetchArticlesByUrls(urls);
+  }
+
+  private async bulkCreateArticles(items: FeedItem[]) {
+    const articles = items.map((item) => ({
+      media: this.media,
+      title: item.title,
+      author: item.author,
+      description: item.description,
+      url: item.url,
+    }));
+
+    return await this.repository.bulkCreateArticle(articles);
   }
 }
