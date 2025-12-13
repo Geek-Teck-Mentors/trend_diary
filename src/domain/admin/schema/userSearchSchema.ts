fa374b45f1e0b71fd651abe43dda3ac499476b86
rin2yh <hyuki749@gmail.com>
2025-12-13 13:51:11 +0900

    fix: domainからも削除

--- src/domain/admin/schema/userSearchSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const userSearchQuerySchema = z.object({
-  searchQuery: z.string().optional(),
-  page: z.number().int().positive().optional(),
-  limit: z.number().int().positive().optional(),
-})
-
-export type UserSearchQuery = z.infer<typeof userSearchQuerySchema>
