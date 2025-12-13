65bbd318e71d573747b359390aedf67bf5127e0e
rin2yh <hyuki749@gmail.com>
2025-12-13 13:50:21 +0900

    fix: APIの不要な機能削除

--- src/application/api/route.ts
+++ src/application/api/route.ts
@@ -1,9 +1,5 @@
 import { Hono } from 'hono'
-import adminApp from '@/application/api/admin/route'
 import articleApp from '@/application/api/article/route'
-import endpointsApp from '@/application/api/endpoints/route'
-import permissionsApp from '@/application/api/permissions/route'
-import policyApp from '@/application/api/policy/route'
 import rolesApp from '@/application/api/roles/route'
 import userApp from '@/application/api/user/route'
 import authV2App from '@/application/api/v2/auth/route'
@@ -11,11 +7,7 @@ import authV2App from '@/application/api/v2/auth/route'
 const app = new Hono()
   .route('/user', userApp)
   .route('/articles', articleApp)
-  .route('/policies', policyApp)
-  .route('/admin', adminApp)
   .route('/roles', rolesApp)
-  .route('/permissions', permissionsApp)
-  .route('/endpoints', endpointsApp)
   .route('/v2/auth', authV2App)
 
 export default app
