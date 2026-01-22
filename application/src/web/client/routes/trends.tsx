4dafd8ef944296fbe1f05f2acf1310cda8fb7cd2
kouphasi <phasi.ok.phasi@gmail.com>
2026-01-23 00:50:39 +0900

    feat: featureFlagを一応mount処理の依存配列に追加

--- application/src/web/client/routes/trends.tsx
+++ application/src/web/client/routes/trends.tsx
@@ -39,7 +39,7 @@ export default function Layout() {
     return () => {
       isMounted = false
     }
-  }, [])
+  }, [userFeatureEnabled])
 
   const outletContext: TrendsOutletContext = {
     userFeatureEnabled,
